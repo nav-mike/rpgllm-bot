ed4a04087bb8be4680786192b4f0eb17ee71df98
Mikhail Navrotskii <mikhail.navrotskii@zevoy.com>
2026-06-09 10:07:37 +0300

    feat: add basic bot boilerplate

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from rpgllm-bot!")
+import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm bot, please talk to me!"
+    )
 
 
 if __name__ == "__main__":
-    main()
+    application = ApplicationBuilder().token(os.environ["TELEGRAM_BOT_TOKEN"]).build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
